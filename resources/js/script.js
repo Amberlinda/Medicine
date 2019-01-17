f0019240494a23e12e5921ed9397849d08401536
Amberlinda <aul123linda@gmail.com>
2019-01-17 20:07:58 +0530

    First commit

---
+++ resources/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    $('.js--section-genres').waypoint(function (direction) {
+
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+
+    });
+    /*Search button animation*/
+    
+    $('.js--search').click(function(){
+       $('.js--search-text').fadeIn(500); 
+    });
+    
+     /*Register button animation*/
+    
+    $('.js--register').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--wp-1').offset().top
+        }, 1000);
+    });
+    
+    
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+});
